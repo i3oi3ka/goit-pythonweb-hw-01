fe8edb110200b1e749f38860722b8a1bae08978d
i3oi3ka <i3oi3ka@ukr.net>
2025-08-05 23:01:11 +0300

    Implement vehicle factory pattern with US and EU factories, and add logging for engine start

---
+++ task1.py
@@ -0,0 +1,67 @@
+import logging
+from abc import ABC, abstractmethod
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start_engine(self, make, model):
+        pass
+
+
+class VehicleFactory(ABC):
+    @abstractmethod
+    def create_car(self, make, model):
+        pass
+
+    @abstractmethod
+    def create_motorcycle(self, make, model):
+        pass
+
+
+class USVehicleFactory(VehicleFactory):
+
+    def create_car(self, make, model):
+        return Car(make, f"{model} (US Spec)")
+
+    def create_motorcycle(self, make, model):
+        return Motorcycle(make, f"{model} (US Spec)")
+
+
+class EUVehicleFactory(VehicleFactory):
+
+    def create_car(self, make, model):
+        return Car(make, f"{model} (EU Spec)")
+
+    def create_motorcycle(self, make, model):
+        return Motorcycle(make, f"{model} (EU Spec)")
+
+
+class Car(Vehicle):
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def start_engine(self):
+        logging.info(f"{self.make} {self.model}: Двигун запущено")
+
+
+class Motorcycle(Vehicle):
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def start_engine(self):
+        logging.info(f"{self.make} {self.model}: Мотор заведено")
+
+
+USVehicleFactory = USVehicleFactory()
+EUVehicleFactory = EUVehicleFactory()
+
+# Використання
+vehicle1 = USVehicleFactory.create_car("Toyota", "Corolla")
+vehicle1.start_engine()
+
+vehicle2 = EUVehicleFactory.create_motorcycle("Harley-Davidson", "Sportster")
+vehicle2.start_engine()
